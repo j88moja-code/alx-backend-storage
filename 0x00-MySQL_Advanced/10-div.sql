1698bf36b9c9c3f095e9dedad1260c4751a116d8
j88moja-code <sitholent+github@gmail.com>
2023-04-06 14:49:45 +0000

    10. Safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- Function that safely divides two numbers
+-- without raising the zero error.
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER //
+CREATE FUNCTION SafeDiv (a INT, b INT)
+    RETURNS FLOAT
+    BEGIN
+        DECLARE result FLOAT;
+        IF b = 0 THEN
+            SET result = 0;
+        ELSE
+            SET result = a / b;
+        END IF;
+        RETURN result;
+    END //
+DELIMITER ;
